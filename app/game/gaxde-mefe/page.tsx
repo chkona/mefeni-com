ba9c3aadd8d09a2d2110a490380e3c1756e1e85d
chkona <chkonidzenodari6@gmail.com>
2026-07-21 12:43:25 +0400

    Create page.tsx

---
+++ app/game/gaxde-mefe/page.tsx
@@ -0,0 +1,10 @@
+import GameRunner from "@/components/game/GameRunner";
+
+export default function GamePage() {
+  return (
+    <section className="max-w-4xl mx-auto px-6 py-16">
+      <GameRunner />
+    </section>
+  );
+}
+
