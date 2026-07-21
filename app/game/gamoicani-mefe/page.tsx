b2fda4e830eb1aff29488865a74c34a6bf48eb87
chkona <chkonidzenodari6@gmail.com>
2026-07-21 12:43:46 +0400

    Create page.tsx

---
+++ app/game/gamoicani-mefe/page.tsx
@@ -0,0 +1,10 @@
+import GuessKingRunner from "@/components/guessgame/GuessKingRunner";
+
+export default function GuessKingPage() {
+  return (
+    <section className="max-w-4xl mx-auto px-6 py-16">
+      <GuessKingRunner />
+    </section>
+  );
+}
+
