bb441afbad33fcba3335a215883b576364305d74
chkona <chkonidzenodari6@gmail.com>
2026-08-18 19:31:08 +0400

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,7 +7,7 @@ import { Analytics } from "@vercel/analytics/next";
 import { SpeedInsights } from "@vercel/speed-insights/next";
 
 export const metadata: Metadata = {
-  title: "მეფენი  — ყველა მეფე ერთად",
+  title: "მეფენი.GE  — ყველა მეფე ერთად",
   description:
     "ქართველ მეფეთა,ისტორია — ფარნავაზიდან გიორგი XII-მდე.",
 };
